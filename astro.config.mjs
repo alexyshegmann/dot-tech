9901f90d485f7bf5686e9ed594567b87a6fd919a
Alexys Hegmann <159317602+alexyshegmann@users.noreply.github.com>
2025-09-04 12:54:39 -0600

    feature: Add related posts to single post pages (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,19 @@ import pagefind from "astro-pagefind";
 import icon from 'astro-icon';
 import preact from '@astrojs/preact';
 
+import relatinator from 'astro-relatinator';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexyshegmann.tech",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [astroReadingTime(), icon(), pagefind(), preact({ devtools: true })],
+  integrations: [astroReadingTime(), icon(), pagefind(), preact({ devtools: true }), relatinator({
+    paths: ["./src/content/blog"],
+    schema: ["title", "excerpt", "tags"],
+    similarityMethod: "bm25",
+  })],
   markdown: {
     shikiConfig: {
       theme: 'dracula'
